48cebb16c3a4da78e75da5ca5ef36b2f1eebd14e
lqx <24210240085@m.fudan.edu.cn>
2025-02-16 12:16:49 +0800

    用户服务完成

--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -1,8 +1,9 @@
 package rpc
 
 import (
-	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/kitex/client"
+	"github.com/feeeeling/eMall/app/frontend/conf"
+	frontendUtils "github.com/feeeeling/eMall/app/frontend/utils"
 	"github.com/feeeeling/eMall/rpc_gen/kitex_gen/user/userservice"
 	consul "github.com/kitex-contrib/registry-consul"
 	"sync"
@@ -21,12 +22,8 @@ func Init() {
 }
 
 func initUserClient() {
-	r, err := consul.NewConsulResolver("127.0.0.1:8500")
-	if err != nil {
-		hlog.Fatal(err)
-	}
+	r, err := consul.NewConsulResolver(conf.GetConf().Hertz.RegistryAddr)
+	frontendUtils.MustHandleError(err)
 	UserClient, err = userservice.NewClient("user", client.WithResolver(r))
-	if err != nil {
-		hlog.Fatal(err)
-	}
+	frontendUtils.MustHandleError(err)
 }
--- app/frontend/middleware/auth.go
+++ app/frontend/middleware/auth.go
@@ -3,22 +3,15 @@ package middleware
 import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
+	frontendUtils "github.com/feeeeling/eMall/app/frontend/utils"
 	"github.com/hertz-contrib/sessions"
-)
-
-type SessionUserIdKey string
-
-const (
-	SessionUserId SessionUserIdKey = "user_id"
 )
 
 func GlobalAuth() app.HandlerFunc {
 	return func(ctx context.Context, c *app.RequestContext) {
 		// TODO: 鉴权
-		//klog.Info("middleware GlobalAuth start")
 		session := sessions.Default(c)
-		ctx = context.WithValue(ctx, SessionUserId, session.Get("user_id"))
-		//klog.Infof("middleware GlobalAuth session: %v", ctx.Value(SessionUserId))
+		ctx = context.WithValue(ctx, frontendUtils.SessionUserId, session.Get("user_id"))
 		c.Next(ctx)
 	}
 }
